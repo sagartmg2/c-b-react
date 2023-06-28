42b015b64dabccb1e414d2a6f12c8adb48f58ff9
Sagar <dev.sagartmg@gmail.com>
2023-06-28 17:44:37 +0545

    react setup

---
+++ demo/src/Navbar.jsx
@@ -0,0 +1,14 @@
+
+function Navbar() {
+    return <header>
+        <nav>
+            <ul>
+                <li><a>home</a></li>
+                <li><a>about</a></li>
+                <li><a>contact</a></li>
+            </ul>
+        </nav>
+    </header>
+}
+
+export default Navbar
