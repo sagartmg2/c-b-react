ba5954a0d2c4c9bcd4c34622c70bc1e574f17828
Sagar <dev.sagartmg@gmail.com>
2023-07-12 17:30:47 +0545

    project setup with tailwind

---
+++ ecommerce-client/src/App.jsx
@@ -0,0 +1,17 @@
+import { AiOutlineMail } from "react-icons/ai";
+
+
+function App() {
+
+  return (
+    <>
+      <header>
+        < AiOutlineMail class  /> em@em.com
+
+
+      </header>
+    </>
+  )
+}
+
+export default App
